3f83e76b411d9d31f3548bd133ea8171a018d5a7
Alex Ortiz <alexormx@gmail.com>
2022-07-09 11:28:00 -0500

    Add Authentification and Session

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              "smpt.gmail.com",
+    port:                 587,
+    domain:               "domain.of.sender.net",
+    authentification:     "plain",
+    user_name:            "alexormx",
+    password:             "Alex1984..",
+    enable_starttls_auto: true
+  }
+end
